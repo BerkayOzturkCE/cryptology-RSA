dd4f371c6ffc76a1de9ea3e5d048ecc9bd469b68
Berkay Ozturk <berkayozturkCE@gmail.com>
2021-04-01 22:03:33 +0300

    first commit

---
+++ cryptology/kriptoloji proje/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace kriptoloji_proje
+{
+    partial class form
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metin1 = new System.Windows.Forms.RichTextBox();
+            this.metin2 = new System.Windows.Forms.RichTextBox();
+            this.BtnSfrle = new System.Windows.Forms.Button();
+            this.BtnCoz = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // metin1
+            // 
+            this.metin1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.metin1.Location = new System.Drawing.Point(12, 113);
+            this.metin1.Name = "metin1";
+            this.metin1.Size = new System.Drawing.Size(435, 390);
+            this.metin1.TabIndex = 0;
+            this.metin1.Text = "";
+            // 
+            // metin2
+            // 
+            this.metin2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.metin2.Location = new System.Drawing.Point(599, 113);
+            this.metin2.Name = "metin2";
+            this.metin2.ReadOnly = true;
+            this.metin2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.metin2.Size = new System.Drawing.Size(435, 390);
+            this.metin2.TabIndex = 1;
+            this.metin2.Text = "";
+            // 
+            // BtnSfrle
+            // 
+            this.BtnSfrle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.BtnSfrle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnSfrle.Location = new System.Drawing.Point(466, 113);
+            this.BtnSfrle.Name = "BtnSfrle";
+            this.BtnSfrle.Size = new System.Drawing.Size(116, 50);
+            this.BtnSfrle.TabIndex = 2;
+            this.BtnSfrle.Text = "Encrypt";
+            this.BtnSfrle.UseVisualStyleBackColor = true;
+            this.BtnSfrle.Click += new System.EventHandler(this.BtnSfrle_Click);
+            // 
+            // BtnCoz
+            // 
+            this.BtnCoz.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.BtnCoz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnCoz.Location = new System.Drawing.Point(466, 203);
+            this.BtnCoz.Name = "BtnCoz";
+            this.BtnCoz.Size = new System.Drawing.Size(116, 50);
+            this.BtnCoz.TabIndex = 3;
+            this.BtnCoz.Text = "Decrypt";
+            this.BtnCoz.UseVisualStyleBackColor = true;
+            this.BtnCoz.Click += new System.EventHandler(this.BtnCoz_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(135, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(734, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output";
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1043, 542);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCoz);
+            this.Controls.Add(this.BtnSfrle);
+            this.Controls.Add(this.metin2);
+            this.Controls.Add(this.metin1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "form";
+            this.Text = "RSA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox metin1;
+        private System.Windows.Forms.Button BtnSfrle;
+        private System.Windows.Forms.Button BtnCoz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox metin2;
+    }
+}
+
---
+++ cryptology/kriptoloji proje/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Numerics;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+
+namespace kriptoloji_proje
+{
+    public partial class form : Form
+    {
+        //RSA için gereken global değişkenler
+
+        int p = 11, q = 197; //p ve q asal sayılarını tanımladık.
+        int e = 107; //public key tanımlandı.
+        int totient, n;
+
+
+        public form()
+        {
+            InitializeComponent();
+        }   
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            totient = (p - 1) * (q - 1); // totient sayısı hesaplandı.
+             n = p * q; // n base değerini bulduk.
+        }
+
+        private void BtnSfrle_Click(object sender, EventArgs e)
+        {
+          
+                RSA_Encryption();
+           
+        }
+
+        private void BtnCoz_Click(object sender, EventArgs e)
+        {
+           
+                RSA_Decryption();
+           
+        }
+
+        private void RSA_Encryption()
+        {
+
+            String metin = metin1.Text; //şifrelenecek metini aldık.
+            String kriptometin="";
+
+            for(int i = 0; i < metin.Length; i++) //metinin boyutu kadar döngü çalışacak
+            {
+
+                int sayi =Convert.ToInt32(metin[i]); //mesajın ilk indisi int yapıldı
+                int kripto = (int)BigInteger.ModPow(sayi, e, n); // sayi^e mod n işlemi yapıldı
+                kriptometin += Convert.ToChar(kripto); //kripto sayi char yapılıp kripto metine eklendi.
+
+            }
+            metin2.Text = kriptometin; //şifreli mesaj yazdırıldı.
+
+        }
+
+
+        private void RSA_Decryption()
+        {
+           
+            String metin = metin1.Text; //çözülecek metini aldık.
+            String cozulen_mesaj = "";
+            int d = oklid_algorithm(); //uzatılmış oklid algoritması çağırıldı.
+            for (int i = 0; i < metin.Length; i++) //metinin boyutu kadar döngü çalışacak
+            {
+                int sayi = Convert.ToInt32(metin[i]); //sifreli metinin ilk indisi int yapıldı
+                int cozulen = (int)BigInteger.ModPow(sayi, d, n); //sayi^d mod n işlemi yapıldı
+                cozulen_mesaj += Convert.ToChar(cozulen);//cozulen sayi cozulen mesaja eklendi
+            }
+            metin2.Text = cozulen_mesaj; //çözülen metin yazıldı
+            
+        }
+
+
+
+        private int oklid_algorithm() //uzatıılmış öklid algoritması fonksiyonu.
+        {
+            int Q, x1=1, x2=0, x3=totient, y1=0, y2=1, y3=e, t1, t2, t3; //gerekli değişkenleri tanımladık.
+
+            while (y3 != 1) // y3 degeri 1 değilken.
+            {
+              // uzatılmış öklid algoritmasının işlemleri yapılıyor
+                Q = x3 / y3;
+                t1 = x1 - (y1 * Q);
+                t2 = x2 - (y2 * Q);
+                t3 = x3 - (y3 * Q);
+                x1 = y1;
+                x2 = y2;
+                x3 = y3;
+                y1 = t1;
+                y2 = t2;
+                y3 = t3;
+            }
+
+            if (y2 < 0)
+            {
+                y2 += totient;
+            }
+            return y2;
+        }
+    }
+
+}
